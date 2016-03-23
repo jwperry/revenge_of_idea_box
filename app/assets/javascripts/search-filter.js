70da38238716b9fb2fb4754284930c572f4d393c
Joe Perry <joseph.w.perry@gmail.com>
2016-03-23 16:01:15 -0600

    Filering and searching complete

---
+++ app/assets/javascripts/search-filter.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $('#search-field').keyup(function() {
+    var ideas = $('.idea');
+    var term = $('#search-field').val().toLowerCase();
+    
+    if (term === "") {
+      showAllIdeas(ideas);
+    }
+    else {
+      showOnlyMatches(ideas, term);
+    }
+
+  });
+});
+
+function showOnlyMatches(ideas, term) {
+  ideas.each(function() {
+    var  body = $(this).data('idea-body').toLowerCase();
+    var  title = $(this).data('idea-title').toLowerCase();
+    if (body.indexOf(term) === -1 && title.indexOf(term) === -1){
+      $(this).hide();
+    }
+    else {
+      $(this).show();
+    }
+  });
+};
+
+function showAllIdeas(ideas) {
+  ideas.each(function() {
+    $(this).show();
+  });
+};
